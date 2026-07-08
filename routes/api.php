1ba2b6e112b4aeedae553d1cf2edff69121e310f
Jean-Baptiste BANG NTEME <jbang.nteme@gmail.com>
2026-07-08 20:30:14 +0100

    Updated GET and PATCH reservations endpoints for dashboard

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('{vertical}')->middleware('api.key')->group(function () {
     Route::post('/disponibilite', [BookingController::class, 'verifierDisponibilite']);
     Route::post('/reservation',    [BookingController::class, 'creerReservation']);
-    Route::get('{vertical}/reservations', [BookingController::class, 'index']);
-    Route::patch('{vertical}/reservations/{id}', [BookingController::class, 'update']);
+    Route::get('/reservations',           [BookingController::class, 'index']);
+    Route::patch('/reservations/{id}',    [BookingController::class, 'update']);
 });
